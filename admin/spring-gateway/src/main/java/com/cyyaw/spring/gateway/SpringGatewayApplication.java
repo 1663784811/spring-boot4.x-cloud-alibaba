2ab773bc2458aaa7f6bd4de7d90978e32540c699
sandy <1663784811@qq.com>
2026-03-27 15:29:09 +0800

    初始化项目

--- admin/spring-gateway/src/main/java/com/cyyaw/spring/gateway/SpringGatewayApplication.java
+++ admin/spring-gateway/src/main/java/com/cyyaw/spring/gateway/SpringGatewayApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 @Slf4j
 @SpringBootApplication
