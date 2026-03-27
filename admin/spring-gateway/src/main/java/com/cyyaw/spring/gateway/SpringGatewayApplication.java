81acef8fcb831918ba75032e1de190c023a8b338
sandy <1663784811@qq.com>
2026-03-27 14:38:16 +0800

    初始化项目

--- admin/spring-gateway/src/main/java/com/cyyaw/spring/gateway/SpringGatewayApplication.java
+++ admin/spring-gateway/src/main/java/com/cyyaw/spring/gateway/SpringGatewayApplication.java
@@ -1,13 +1,23 @@
 package com.cyyaw.spring.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+@Slf4j
 @SpringBootApplication
 public class SpringGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(SpringGatewayApplication.class, args);
+
+
+
+        log.info("===============   启动成功  ====================");
+        log.info("http://127.0.0.1:9000");
+
+    }
 
 }
