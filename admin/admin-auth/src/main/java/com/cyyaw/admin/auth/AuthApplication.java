47c10e6b5986cf57310c3301ca662a2651d400fe
sandy <1663784811@qq.com>
2026-04-06 11:27:51 +0800

    初始化项目

---
+++ admin/admin-auth/src/main/java/com/cyyaw/admin/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.cyyaw.admin.auth;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
