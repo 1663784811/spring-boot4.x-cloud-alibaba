972123aff2d5eb76659830776e3a7f0668a1461d
sandy <1663784811@qq.com>
2026-04-06 10:51:14 +0800

    初始化项目

--- admin/spring-gateway/src/main/java/com/cyyaw/spring/gateway/SpringGatewayApplication.java
+++ admin/spring-gateway/src/main/java/com/cyyaw/spring/gateway/SpringGatewayApplication.java
@@ -4,19 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
+@LoadBalancerClients({@LoadBalancerClient("spring-gateway")})
 public class SpringGatewayApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(SpringGatewayApplication.class, args);
-
         log.info("===============   启动成功  ====================");
         log.info("http://127.0.0.1:9000");
-
     }
 
 }
